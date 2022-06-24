30bb84d66e1e11a8719efd4ab69f1284dcee6d6e
Sam Heyman <Sam.Heyman@VISSIM.NO>
2022-06-24 14:24:33 +0200

    Add road context with lanes and borders

--- src/roadCanvas.tsx
+++ src/roadCanvas.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Car } from "./car";
+import { Road } from "./road";
 
 interface CanvasProps {
   width: number;
@@ -9,23 +10,34 @@ const RoadCanvas = ({ width = 200 }: CanvasProps) => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
   let ctx: CanvasRenderingContext2D | null;
   let car: Car;
-  const [height, setHeight] = React.useState<number>(window.innerHeight);
+  let road: Road;
 
   const animate = () => {
     car.update();
     ctx!.clearRect(0, 0, width, window.innerHeight);
+    ctx?.save();
+    ctx?.translate(0, -car.y + window.innerHeight * 0.7);
+    road.draw(ctx);
     car.draw(ctx);
+    ctx?.restore();
+
     requestAnimationFrame(animate);
   };
 
   React.useEffect(() => {
     ctx = canvasRef.current ? canvasRef.current.getContext("2d") : null;
+    road = new Road(width / 2, width * 0.9);
     car = new Car(100, 100, 30, 50);
     animate();
   }, []);
 
   return (
-    <canvas id="roadCanvas" ref={canvasRef} width={width} height={height} />
+    <canvas
+      id="roadCanvas"
+      ref={canvasRef}
+      width={width}
+      height={window.innerHeight}
+    />
   );
 };
 
